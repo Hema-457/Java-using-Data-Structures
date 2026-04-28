315c3fa7607773218ca32775a139c0f0cce6ddff
Hema-457 <tamaranahema10@gmail.com>
2026-04-29 00:03:02 +0530

    Create Bubble Sort.java

---
+++ Bubble Sort.java
@@ -0,0 +1,23 @@
+class Main{
+    public static void main(String args[])
+    {
+    int arr[]={69,21,38,29,16,22};
+    for(int i=0;i<arr.length-1;i++)
+    {
+        for(int j=0;j<arr.length-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int x:arr)
+    {
+        System.out.print(x+" ");
+    }
+    
+    }
+}
